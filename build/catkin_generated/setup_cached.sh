adbaca0ad6fb2b15679eb4e4a5270c28061ed3f7
Eren <eren@eren.localdomain>
2024-08-07 15:04:09 +0100

    Initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/eren/ros_stage_task/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/eren/ros_stage_task/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/eren/ros_stage_task/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/eren/ros_stage_task/src:$ROS_PACKAGE_PATH"
