5b95fc19e75f6051bbff33c2fe162fc23f02ce2e
zeynelerenkinali <ernknal@gmail.com>
2024-08-08 11:26:21 +0100

    robot shape designed

--- src/simstage_group2/script/reactive_navigation.py
+++ src/simstage_group2/script/reactive_navigation.py
@@ -4,18 +4,21 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan   
 
 class ReactiveNavigation():
-    def _init_(self):
+    def __init__(self):
+
         self.cmd_vel = Twist()
         self.laser_msg = LaserScan()
-        self.robot_stopped = False
         self.obstacle_distance = 100
+        self.obstacle_threshold = 0.5
+        self.turning = False
+        self.turn_direction = 1
 
-         # Params from config file
-        self.active_robot = str(rospy.get_param("reactive_controller_py/active_robot"))
+        # Params from config file
+        # self.active_robot = str(rospy.get_param("reactive_controller_py/active_robot"))
         
         # Topics
-        self._cmd_topic = self.active_robot+"/cmd_vel"
-        self._laser_topic = self.active_robot+"/base_scan"
+        self._cmd_topic = "cmd_vel"
+        self._laser_topic = "base_scan"
 
         self.rospy_sub_laser = rospy.Subscriber(self._laser_topic, LaserScan, self.laser_cb, queue_size=1)
         self.pub_CMD = rospy.Publisher(self._cmd_topic, Twist, queue_size=1)
@@ -25,24 +28,40 @@ class ReactiveNavigation():
         self.laser_msg = msg
     
     def calculate_command(self):
-        if hasattr(self.laser_msg, 'ranges') and len(self.laser_msg.ranges) > 0:
+        if type(self.laser_msg.ranges) == tuple:
             self.obstacle_distance = min(self.laser_msg.ranges)
 
-            if self.obstacle_distance > 1.0:
-                self.cmd_vel.linear.x = 2.0
-                self.cmd_vel.angular.z = 0.0
+            if self.obstacle_distance > self.obstacle_threshold:
+                if self.turning:
+                    # Finish turning before moving forward
+                    self.cmd_vel.linear.x = 0.0
+                    self.cmd_vel.angular.z = 2.0 * self.turn_direction
+                    if min(self.laser_msg.ranges) > self.obstacle_threshold:
+                        self.turning = False
+                        self.cmd_vel.angular.z = 0.0
+                else:
+                    # Move forward
+                    self.cmd_vel.linear.x = 2.0
+                    self.cmd_vel.angular.z = 0.0
             else:
+                # Stop and turn
                 self.cmd_vel.linear.x = 0.0
-                self.cmd_vel.angular.z = -2.0
+                self.cmd_vel.angular.z = 2.0 * self.turn_direction
+                self.turning = True
+                # Change direction if stuck turning
+                if self.obstacle_distance < self.obstacle_threshold / 2:
+                    self.turn_direction *= -1
+                
+            self.pub_CMD.publish(self.cmd_vel)
+
+                
 
     def run(self):
         while not rospy.is_shutdown():
             self.calculate_command()
-            #print(self.cmd_vel)
-            self.pub_CMD.publish(self.cmd_vel)
             self.rate.sleep()
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     rospy.init_node("reactive_controller_py")
     controller = ReactiveNavigation()
     controller.run()
