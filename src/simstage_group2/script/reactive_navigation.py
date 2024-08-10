d19ae11af09b929b71affe88b22a6a6ace1f477f
zeynelerenkinali <ernknal@gmail.com>
2024-08-10 20:41:31 +0100

    map-search algorithm in progress

--- src/simstage_group2/script/reactive_navigation.py
+++ src/simstage_group2/script/reactive_navigation.py
@@ -10,9 +10,9 @@ class ReactiveNavigation():
         self.laser_msg = LaserScan()
         # Initialization for algorithm
         self.obstacle_distance = 100
-        self.obstacle_threshold = 0.28
-        self.turning = False
-        self.turn_direction = 1
+        self.threshold = 0.8
+        self.forward_speed = 1
+        self.turn_speed = 1
         # Topics
         self._cmd_topic = "cmd_vel"
         self._laser_topic = "base_scan"
@@ -28,9 +28,9 @@ class ReactiveNavigation():
     def calculate_command(self):
         if type(self.laser_msg.ranges) == tuple:
             # Get minimum distance for all directions
-            self.obstacle_distance = min(self.laser_msg.ranges)
+            #self.obstacle_distance = min(self.laser_msg.ranges)
             # Get the total range from length of lidar's data
-            total_ranges = len(self.obstacle_distance)
+            total_ranges = len(self.laser_msg.ranges)
             # Divide the FOV to three and seperate indices to each direction in order to focus information correctly for each direciton.
             one_third = total_ranges // 3
 
@@ -48,11 +48,56 @@ class ReactiveNavigation():
             front_obstacle_distance = min(front_distances)
             left_obstacle_distance = min(left_distances)
             #------------------------
-            # Algorithm to search map
+            # Algorithm to search map *c=close *o=open
             # -----------------------
-            # 1.
-                
-            #self.pub_CMD.publish(self.cmd_vel)
+            #1. If there is a wall on right, front is clear, left is clear: go straight -- coo
+            #2. else if there is a wall on right, front, left is clear: turn left -- cco
+            #3. else if there is a wall on right, front, left: turn backwards --- ccc
+            #4. else if right is clear, front is clear, left is clear: turn place that has more range(r or l) -- ooo
+            #6. else if right is clear, there is a wall on front, left is clear: turn the place that has bigger range of view(r or l sensor has more value) -- oco
+            #7. else if right is clear, front is clear, there is a wall on left: go straight -- ooc
+            #--------code-----------
+            
+            if right_obstacle_distance < self.threshold and front_obstacle_distance >= self.threshold and left_obstacle_distance >= self.threshold:
+                # Case 1: coo - Go straight
+                if right_obstacle_distance < self.threshold / 2:
+                    self.cmd_vel.linear.x = self.forward_speed
+                    self.cmd_vel.angular.z = -self.turn_speed
+                else:
+                    self.cmd_vel.linear.x = self.forward_speed
+                    self.cmd_vel.angular.z = 0.0
+            elif right_obstacle_distance < self.threshold and front_obstacle_distance < self.threshold and left_obstacle_distance >= self.threshold:
+                # Case 2: cco - Turn left
+                self.cmd_vel.linear.x = 0.0
+                self.cmd_vel.angular.z = self.turn_speed
+            elif right_obstacle_distance < self.threshold and front_obstacle_distance < self.threshold and left_obstacle_distance < self.threshold:
+                # Case 3: ccc - Turn backwards
+                self.cmd_vel.linear.x = 0.0
+                self.cmd_vel.angular.z = self.turn_speed
+            elif right_obstacle_distance >= self.threshold and front_obstacle_distance >= self.threshold and left_obstacle_distance >= self.threshold:
+                self.cmd_vel.linear.x = self.forward_speed
+                self.cmd_vel.angular.z = 0.0
+            # elif right_obstacle_distance >= self.threshold and front_obstacle_distance < self.threshold and left_obstacle_distance >= self.threshold:
+            #     # Case 5: oco - Turn to the place with more range (right or left sensor has more value)
+            #     if right_obstacle_distance > left_obstacle_distance:
+            #         # Turn right
+            #         self.cmd_vel.linear.x = 0.0
+            #         self.cmd_vel.angular.z = -self.turn_speed
+            #     else:
+            #         # Turn left
+            #         self.cmd_vel.linear.x = 0.0
+            #         self.cmd_vel.angular.z = self.turn_speed
+            elif right_obstacle_distance >= self.threshold and front_obstacle_distance >= self.threshold and left_obstacle_distance < self.threshold:
+                # Case 6: ooc - Go straight
+                if left_obstacle_distance < self.threshold / 2:
+                    self.cmd_vel.linear.x = self.forward_speed
+                    self.cmd_vel.angular.z = self.turn_speed
+                else:
+                    self.cmd_vel.linear.x = self.forward_speed
+                    self.cmd_vel.angular.z = 0.0
+
+            # Publish the command
+            self.pub_CMD.publish(self.cmd_vel)
             
     def run(self):
         while not rospy.is_shutdown():
